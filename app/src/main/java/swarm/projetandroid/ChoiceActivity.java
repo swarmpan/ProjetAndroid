b7fb75c889bbab97f12db03c50c9173e12baa300
Swarm <spamswarm2@gmail.com>
2016-04-06 18:17:34 +0200

    Google key ChoiceActivity

---
+++ app/src/main/java/swarm/projetandroid/ChoiceActivity.java
@@ -0,0 +1,13 @@
+package swarm.projetandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+    }
+}
